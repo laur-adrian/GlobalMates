65d27437d1dc8d4bce30c1cf5b1dc76fa39dc254
Delia0106 <deliagheorghe0106@gmail.com>
2024-05-19 22:49:11 +0300

    Add style + created all frontend components

---
+++ src/main/webapp/app/matches/matches.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-matches',
+  templateUrl: './matches.component.html',
+  styleUrls: ['./matches.component.scss'],
+})
+export class MatchesComponent {
+  // constructor() { }
+  //
+  // ngOnInit(): void {
+  // }
+}
---
+++ src/main/webapp/app/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent {
+  //
+  // constructor() { }
+  //
+  // ngOnInit(): void {
+  // }
+}
